37711035cb1753a94117b2d74153e3d50a3f9451
Hung Vu <hung.vu@tiki.vn>
2022-07-30 16:14:58 +0700

    Update contract

---
+++ contracts/lib/Signature.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Signature {
+
+    function splitSignature(bytes memory sig) private pure returns (uint8 v, bytes32 r, bytes32 s) {
+        require(sig.length == 65);
+
+        assembly {
+            // first 32 bytes, after the length prefix.
+            r := mload(add(sig, 32))
+            // second 32 bytes.
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes).
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
+    }
+
+    function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    function prefixed(bytes32 msgHash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", msgHash));
+    }
+
+    /**
+     * @dev Make sure all signatures and signers are valid
+     */
+    function verifySignatures(bytes32 msgHash, bytes[] memory signatures, address[] memory signers) internal pure {
+        require(signatures.length == signers.length, "INVALID_LENGTH_SIG");
+
+        for(uint i = 0; i < signatures.length; i++ ) {
+            bytes32 message = prefixed(msgHash);
+            require(recoverSigner(message, signatures[i]) == signers[i], "INVALID_SIGNATURE");
+        }
+    }
+
+}
--- migrations/1_nft_migration.js
+++ migrations/1_nft_migration.js
@@ -1,12 +1,16 @@
-var UniqueNFT = artifacts.require("UniqueNFT");
-var MultiNFT = artifacts.require("MultiNFT");
+var T2WebNFT = artifacts.require("T2WebNFT");
+var T2WebMultiNFT = artifacts.require("T2WebMultiNFT");
+var T2WebERC4907NFT = artifacts.require("T2WebERC4907NFT");
 
 var BASE_URI = process.env.BASE_URI || "https://t2web.mypinata.cloud/ipfs/";
 
 module.exports = function (deployer) {
-  // console.log("Deploying UniqueNFT...");
-  // deployer.deploy(UniqueNFT, "T2WEB NFT", "T2WEB", BASE_URI);
+  // console.log("Deploying T2WebNFT...");
+  // deployer.deploy(T2WebNFT, "T2WEB NFT", "T2WEB", BASE_URI);
 
-  // console.log("Deploying MultiNFT...");
-  // deployer.deploy(MultiNFT, BASE_URI, "");
+  // console.log("Deploying T2WebMultiNFT...");
+  // deployer.deploy(T2WebMultiNFT, BASE_URI, "");
+
+  console.log("Deploying T2WebERC4907NFT...");
+  deployer.deploy(T2WebERC4907NFT, "T2WEB NFT", "T2WEB", BASE_URI);
 };
